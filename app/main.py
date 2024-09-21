bdf30c8082622ed360717668da32f2428ae08841
rimorcoder <181167401+rimorcoder@users.noreply.github.com>
2024-09-21 12:54:49 -0400

    init rate limiting

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
 from redis import Redis, ConnectionPool
 from pydantic import BaseModel
 import json
@@ -36,37 +37,67 @@ class WeatherData(BaseModel):
     tzoffset: float
     days: List[DayForecast]
 
+
+@app.middleware("http")
+async def rate_limit_middleware(request: Request, call_next):
+    client_ip = request.client.host
+    endpoint = request.url.path
+    key = f"rate_limit:{client_ip}:{endpoint}"
+    rate_limit = 5  # Max requests
+    time_window = 60  # Time window in seconds
+
+    request_count = redis_client.get(key)
+
+    if request_count is None:
+        redis_client.setex(key, time_window, 1)
+    else:
+        request_count = int(request_count)
+        if request_count < rate_limit:
+            redis_client.incr(key)
+        else:
+            logger.warning(f"Rate limit exceeded for client: {client_ip} | {endpoint}")
+            return JSONResponse(status_code=500, content={"detail": "Rate limit exceeded"})
+    
+    response = await call_next(request)
+    return response
+
+    
 @app.get("/")
 async def read_root():
     return {"message": "weather api"}
 
 @app.get("/weather/{address}", response_model=WeatherData)
 async def get_weather(address: str, request: Request):
-    cached_weather = redis_client.get(f"record:{address}")
+    try:
+        cached_weather = redis_client.get(f"record:{address}")
 
-    if cached_weather:
-        weather_data = json.loads(cached_weather)
-        weather = WeatherData(**weather_data)
-        logger.info("data retrieved from cache")  
-        return weather
-    else:
-        api_url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{address}"
-        response = httpx.get(api_url, params={
-            'key': os.getenv('API_KEY'),
-            'unitGroup': 'us',
-            'include': 'current',
-            'contentType': 'json',
-            'include': 'days',
-            'elements': 'datetime,tempmax,tempmin,temp'
-        })
-                
+        if cached_weather:
+            weather_data = json.loads(cached_weather)
+            weather = WeatherData(**weather_data)
+            logger.info("data retrieved from cache")  
+            return weather
+        else:
+            api_url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{address}"
+            async with httpx.AsyncClient() as client:
+                response = await client.get(api_url, params={
+                    'key': os.getenv('API_KEY'),
+                    'unitGroup': 'us',
+                    'include': 'current',
+                    'contentType': 'json',
+                    'include': 'days',
+                    'elements': 'datetime,tempmax,tempmin,temp'
+                })
+                    
 
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        weather_data = response.json()
-        weather = WeatherData(**weather_data)
-        redis_client.set(f"record:{address}", json.dumps(weather.dict())) 
-        logger.info("data added to cache")  
-        return weather
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            weather_data = response.json()
+            weather = WeatherData(**weather_data)
+            redis_client.set(f"record:{address}", json.dumps(weather.dict())) 
+            logger.info("data added to cache")  
+            return weather
+    except Exception as e:
+        logger.error(e, exc_info=True)
+        return JSONResponse(status_code=500, content={"detail": "Internal server error"})
     
 @app.delete("/weather/{address}")
 async def delete_weather(address: str, request: Request):
